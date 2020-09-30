b5c90c4d97e71d0b1a6c2125c1a73315ef769b91
Oluwasegun Ojo <oluwasegun.ojo@imdea.org>
2020-10-01 00:39:30 +0200

    add regional ccfr

--- scripts/ccfr.R
+++ scripts/ccfr.R
@@ -1,13 +1,45 @@
 library(dplyr)
 
-dtcases <- read.csv("../data/datadista_regional/ccaa_covid19_datos_isciii_nueva_serie.csv", as.is = T)
-dtcases <- dtcases %>% select(fecha, cod_ine, ccaa, num_casos) %>% rename(cases = num_casos)
+###################################################################
+calculate_ci <- function(p_est, level, pop_size) {
+  z <- qnorm(level+(1-level)/2)
+  se <- sqrt(p_est*(1-p_est))/sqrt(pop_size)
+  return(list(low=p_est-z*se, upp=p_est+z*se, error=z*se))
+}
 
-dtdeaths <- read.csv("../data/datadista_regional/ccaa_covid19_fallecidos_por_fecha_defuncion_nueva_serie_long.csv",
-                     as.is = T)
-dtdeaths <- dtdeaths %>% rename(ccaa = CCAA, deaths = total)
+hosp_to_death_trunc <- function(x, mu_hdt, sigma_hdt){
+  dlnorm(x, mu_hdt, sigma_hdt)
+}
 
-data <- full_join(dtcases, dtdeaths, by = c("fecha", "cod_ine", "ccaa"))
+scale_cfr <- function(data_1_in, delay_fun, mu_hdt, sigma_hdt){
+  case_incidence <- data_1_in$cases
+  death_incidence <- data_1_in$deaths
+  cumulative_known_t <- 0 # cumulative cases with known outcome at time tt
+  # Sum over cases up to time tt
+  for(ii in 1:length(case_incidence)){
+    known_i <- 0 # number of cases with known outcome at time ii
+    for(jj in 0:(ii - 1)){
+      known_jj <- case_incidence[ii - jj] * delay_fun(jj, mu_hdt = mu_hdt, sigma_hdt = sigma_hdt)
+      known_i <- known_i + known_jj
+    }
+    cumulative_known_t <- cumulative_known_t + known_i # Tally cumulative known
+  }
+  # naive CFR value
+  b_tt <- sum(death_incidence)/sum(case_incidence) 
+  # corrected CFR estimator
+  p_tt <- sum(death_incidence)/cumulative_known_t
+  if (sum(death_incidence, na.rm = T) > cumulative_known_t){
+    ccfrr <- data.frame(nCFR = 0, cCFR = 0, total_deaths = 0, 
+                        cum_known_t = 0, total_cases = sum(case_incidence))
+  } else{
+    ccfrr <- data.frame(nCFR = b_tt, cCFR = p_tt, total_deaths = sum(death_incidence), 
+                        cum_known_t = round(cumulative_known_t), total_cases = sum(case_incidence))
+  }
+  return(ccfrr)
+}
+#############################################################################
+
+
 
 plot_estimates <- function(region_ine = 1,
                            z_mean_hdt = 13,
@@ -17,7 +49,7 @@ plot_estimates <- function(region_ine = 1,
                            c_cfr_estimate_range = c(1.23, 1.53),
                            dts = data, 
                            ac_window){
-  cat("::- script-ccfr-based: Computing ccfr-based estimates for", country_geoid, "::\n")
+  #cat("::- script-ccfr-based: Computing ccfr-based estimates for", country_geoid, "::\n")
   mu_hdt = log(z_median_hdt)
   sigma_hdt = sqrt(2*(log(z_mean_hdt) - mu_hdt))
   
@@ -36,12 +68,12 @@ plot_estimates <- function(region_ine = 1,
   dt$cum_deaths <- cumsum(dt$deaths)
   
   dt$date <- as.Date(dt$fecha, format = "%Y-%m-%d")
-  dt$fecha <- gsub("-", "/", dt$date)
+  #dt$fecha <- gsub("-", "/", dt$date)
   
-  dt <- data %>% 
-    select(date, cases, deaths, cum_cases, cum_deaths) %>% 
-    rename(population = popData2019) %>% 
-    select(date, cases, deaths, cum_cases, cum_deaths, population)
+  # dt <- dt %>% 
+  #   select(date, cases, deaths, cum_cases, cum_deaths) %>% 
+  #   rename(population = popData2019) %>% 
+  #   select(date, cases, deaths, cum_cases, cum_deaths, population)
   
   
   ndt <- nrow(dt)
@@ -98,57 +130,44 @@ plot_estimates <- function(region_ine = 1,
   
   
   dt_w <- dt %>% 
-    select("date", "cases", "deaths", "cum_cases", "cum_deaths", "cases_daily", "cases_active", "cases_active_undected", 
+    select("date", "ccaa", "reg_code", "cases", "deaths", "cum_cases", "cum_deaths", "cases_daily", "cases_active", "cases_active_undected", 
            "p_cases", "p_cases_low", "p_cases_high", "p_cases_daily", "p_cases_active", "p_cases_active_undetected", 
            "population")
   
-  dir.create("../data/estimates-ccfr-based/PlotData/", showWarnings = F)
-  cat("::- script-ccfr-based: Writing data for", country_geoid, "::\n")
-  write.csv(dt_w, paste0("../data/estimates-ccfr-based/PlotData/", country_geoid, "-estimate.csv"))
+  dir.create("../data/estimates-ccfr-based/spain/", showWarnings = F)
+  cat("::- script-ccfr-based: Writing data for", dt$ccaa[1], "::\n")
+  write.csv(dt_w, paste0("../data/estimates-ccfr-based/spain/", dt$ccaa[1], "-estimate.csv"))
   
 } 
 
 
+
+
+
+
+
 generate_estimates <- function(active_window_cases = 12){
-  url <- paste("https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-",
-               Sys.Date(), ".xlsx", sep = "")
-  GET(url, authenticate(":", ":", type="ntlm"), write_disk(tf <- tempfile(fileext = ".xlsx")))
-  cat("::- script-ccfr: Checking the ECDC data for the day ::\n")
-  #try( data_ecdc <- read_excel(tf), silent = T) # ECDC daily excel seems unvailable for now
-  try( data_ecdc <- read.csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv",
-                             na.strings = "", fileEncoding = "UTF-8-BOM"), silent = T)
+  casesurl <- "https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/ccaa_covid19_datos_isciii_nueva_serie.csv"
+  deathsurl <- "https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/ccaa_covid19_fallecidos_por_fecha_defuncion_nueva_serie_long.csv"
+  dtcases <- read.csv(casesurl, as.is = T)
+  dtcases <- dtcases %>% select(fecha, cod_ine, ccaa, num_casos) %>%
+    rename(cases = num_casos)
+  dtdeaths <- read.csv(deathsurl, as.is = T)
+  dtdeaths <- dtdeaths %>%
+    rename(ccaa = CCAA, deaths = total)
+  ine_dict <- data.frame(cod_ine = 1:19, 
+                         reg_code = c("ESAN", "ESAR", "ESAS", "ESIB", "ESCN", "ESCB", "ESCL",
+                                      "ESCM", "ESCT", "ESVC", "ESEX", "ESGA", "ESMD",
+                                      "ESMC", "ESNC", "ESPV", "ESRI", "ESCE", "ESML"))
+  regsdata <- read.csv("../data/common_data/regions-tree-population.csv", as.is = T) %>% 
+    filter(countrycode == "ES") %>% 
+    group_by(regioncode) %>% 
+    summarise(population = sum(population))
   
-  if(!exists("data_ecdc")){
-    cat("::- script-ccfr: Seems the ECDC data for the day is not available yet ::\n")
-    cat("::- script-ccfr: Trying to get data for the previous day ::\n")
-    url <- paste("https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-",
-                 Sys.Date()-1, ".xlsx", sep = "")
-    GET(url, authenticate(":", ":", type="ntlm"), write_disk(tf <- tempfile(fileext = ".xlsx")))
-    try( data_ecdc <- read_excel(tf), silent = T)
-    if(!exists("data_ecdc")){
-      stop("::- script-confirmed: Unfortunately, the ECDC data for yesterday is not availabe neither ::\n")
-    }else{
-      cat("::- script-ccfr: Using ECDC data for previous day ::\n")
-      data_ecdc$countryterritoryCode[data_ecdc$geoId == "CZ"] <- "CZE" # add "CZ" manually
-      data_country_code <- read_excel("../data/common-data/wikipedia-iso-country-codes.xlsx")
-      names(data_country_code) <- c("English.short.name.lower.case", "Alpha.2.code",
-                                    "Alpha.3.code", "Numeric.code", "ISO.3166.2")
-      
-      data_ecdc <- inner_join(data_ecdc, data_country_code, by = c("countryterritoryCode" = "Alpha.3.code"))
-      
-      all_geo_ids <- unique(data_ecdc$Alpha.2.code)
-      sapply(all_geo_ids, plot_estimates, dts = data_ecdc)
-    }
-  } else{
-    cat("::- script-ccfr: ECDC data for the day available! ::\n")
-    data_ecdc$countryterritoryCode[data_ecdc$geoId == "CZ"] <- "CZE" # add "CZ" manually
-    data_country_code <- read_excel("../data/common-data/wikipedia-iso-country-codes.xlsx")
-    names(data_country_code) <- c("English.short.name.lower.case", "Alpha.2.code",
-                                  "Alpha.3.code", "Numeric.code", "ISO.3166.2")
-    data_ecdc <- inner_join(data_ecdc, data_country_code, by = c("countryterritoryCode" = "Alpha.3.code"))
-    all_geo_ids <- unique(data_ecdc$Alpha.2.code) 
-    go <- sapply(all_geo_ids, plot_estimates, dts =  data_ecdc, ac_window = active_window_cases)
-  }
+  data <- full_join(dtcases, dtdeaths, by = c("fecha", "cod_ine", "ccaa")) %>% 
+    left_join(ine_dict, by = "cod_ine") %>% left_join(regsdata, by = c("reg_code" = "regioncode"))
+  
+  go <- sapply(1:19, plot_estimates, dts =  data, ac_window = active_window_cases)
   
 }
 generate_estimates()
