cd3d0cc87280721542f91256ebda542a0bd834b7
CBaquero <cbm@di.uminho.pt>
2020-11-22 15:59:27 +0000

    prediction

--- scripts/script-symp-to-death-lag.R
+++ scripts/script-symp-to-death-lag.R
@@ -2,15 +2,17 @@
 library(dplyr)
 library(stringr)
 library(zoo) # to use rollmean
+library(foreign)
+library(MASS)
 library(ggplot2)
 
-iso_code_country <- "ES"
+iso_code_country <- "FR"
 
 ## Load UMD regressors ----
 
-# iso_code_country <- "BR"
+#data_df <-  read.csv(paste0("../data/estimates-umd-batches/", iso_code_country , "/", iso_code_country ,"_UMD_country_data.csv"))
+data_df <-  read.csv(paste0("../data/estimates-umd-unbatched/PlotData/", iso_code_country ,"_UMD_country_nobatch_past_smooth.csv"))
 
-data_df <-  read.csv(paste0("../data/estimates-umd-batches/", iso_code_country , "/", iso_code_country ,"_UMD_country_data.csv"))
 
 data_df$date <- as.Date(data_df$date)
 
@@ -27,9 +29,11 @@ colnames(df_umd)
 
 ## Load CCFR regressors
 
-df_ccfr <- read.csv(paste0("../data/estimates-ccfr-based/PlotData/", iso_code_country,"-estimate.csv")) %>% 
+#df_ccfr <- read.csv(paste0("../data/estimates-ccfr-based/PlotData/", iso_code_country,"-estimate.csv")) %>% 
+#  mutate(date = as.Date(date) )
+df_ccfr <- read.csv(paste0("../../coronasurveys/coronasurveys/data/estimates-ccfr-based/PlotData/", iso_code_country,"-estimate.csv")) %>% 
   mutate(date = as.Date(date) )
-
+  
 ## Load NSUM regressors TODO
 
 #df_nsum <- read.csv(paste0("../data/estimates-ccfr-based/PlotData/", iso_code_country,"-estimate.csv")) %>% 
@@ -37,13 +41,14 @@ df_ccfr <- read.csv(paste0("../data/estimates-ccfr-based/PlotData/", iso_code_co
 
 ## Load number of deaths ----  
 
-df_deaths <- read.csv(paste0("../data/estimates-ccfr-based/PlotData/", iso_code_country,"-estimate.csv")) %>% 
+#df_deaths <- read.csv(paste0("../data/estimates-ccfr-based/PlotData/", iso_code_country,"-estimate.csv")) %>% 
+df_deaths <- read.csv(paste0("../../coronasurveys/coronasurveys/data/estimates-ccfr-based/PlotData/", iso_code_country,"-estimate.csv")) %>% 
   mutate(date = as.Date(date) ) %>% 
   mutate(y = deaths) %>% 
-  mutate(y = rollmean(y, 7, fill = NA)) %>%
-  filter(!is.na(y)) %>% 
-  select(date, y) %>%
-  filter(date <= "2020-11-04")
+  mutate(y = rollmean(y, 1, fill = NA)) %>%
+  filter(!is.na(y)) #%>% 
+#  select(date, y) %>%
+#  filter(date <= "2020-11-18")
 # 
 # temp_deaths <- df_deaths %>%
 #   select(date, deaths_prev_week) %>%
@@ -89,9 +94,11 @@ check_lags <- function(df_response, df_add_regressors, columns_to_try, date_shif
     win_size <- as.integer(max(df_response$date) - min(df_response$date) )
     
     correl <- cor(df_response$y, df_single_symp[, column_in], method = "spearman" )
+    corTest <- cor.test(df_response$y, df_single_symp[, column_in], method = "spearman" )
     
-    df_correl <- data.frame(shift = date_shift, correlations = correl, 
-                         win_size = win_size,
+    df_correl <- data.frame(shift = date_shift, correlations = corTest$estimate, pval = corTest$p.value,
+
+                        win_size = win_size,
                          signal = column_in)
     
     df_out <- rbind(df_out, df_correl)
@@ -105,14 +112,15 @@ check_lags <- function(df_response, df_add_regressors, columns_to_try, date_shif
 
 
 all_correls <- data.frame()
-for (try_shift in seq(0, 30)) {
+for (try_shift in seq(0, 2*30)) {
   all_correls <- rbind(all_correls, 
                        check_lags(df_deaths, df_umd, 
                                   columns_to_try = c("pct_anosmia_ageusia", 
                                                      "pct_sore_throat", 
                                                      "pct_fever","pct_cmnty_sick"), 
 #                        check_lags(df_deaths, df_ccfr,
-#                                   columns_to_try = c("cases","cases_daily"),
+#                                   columns_to_try = c("cases","cases_daily","cases_active","cases_contagious","deaths"),
+
                                   date_shift = try_shift))
   
 }
@@ -122,6 +130,8 @@ head(all_correls)
 
 p <- ggplot(data = all_correls, aes(x = shift, y = correlations, color = signal)) +
   geom_line(alpha = 0.8) + 
+  ylim(-1,1) +
+#  ylim(0,0.05) +
   labs(title = iso_code_country) +
   theme_light()
 fig <- ggplotly(p)
@@ -138,3 +148,44 @@ fig
 #   theme_light()
 # fig <- ggplotly(p)
 # fig
+
+
+#### NEW
+
+
+lag <- 24 # Choose a good lag by looking at max correlations of predictors
+
+df_umd$date <- as.Date(df_umd$date) + lag
+rend <- min(max(df_umd$date),max(df_deaths$date))
+lend <- max(min(df_umd$date),min(df_deaths$date))
+df_umd_ss <- df_umd %>% 
+  filter(date <= rend) %>%
+  filter(date >= lend)
+df_deaths_ss <- df_deaths %>% 
+  filter(date <= rend) %>%
+  filter(date >= lend)
+
+length(df_deaths_ss$y)
+dados<-data.frame(Y=df_deaths_ss$y, 
+                  P1=df_umd_ss$pct_cmnty_sick, 
+#                  P2=df_umd_ss$pct_anosmia_ageusia,
+                  P3=df_umd_ss$pct_fever,
+                  P4=df_umd_ss$pct_sore_throat)
+
+length(df_deaths_ss$y); lend; rend;
+
+summary(m1 <- glm.nb(Y ~ P1 + P3 + P4, data = dados))
+
+names(m1)
+
+future <- df_umd #%>% filter(date > rend+1)
+
+pred <- data.frame(P1 = future$pct_cmnty_sick, P3 = future$pct_fever, P4 = future$pct_sore_throat)
+pred$Y <- predict(m1, pred, type = "response")
+pred$date <- future$date
+
+sm <- 3 # optional smooth on prediction output
+
+plot(pred$date,runmed(pred$Y,sm),type="b"); points(df_deaths$date,df_deaths$deaths,pch=19, col="red")
+
+
