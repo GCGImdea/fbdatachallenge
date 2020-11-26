c0fda59f4a8e43c8b71dddfdcc70418c76ab0863
hhroig <hhroig@gmail.com>
2020-11-26 15:57:03 +0100

    add deaths forecast by prophet

---
+++ scripts/script-prophet-deaths-by-country.R
@@ -0,0 +1,163 @@
+## Libraries
+library(prophet)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(forecast)
+library(ggplot2)
+library(gridExtra)
+library(zoo)
+library(plotly)
+
+
+propheting_data <- function(df_response, 
+                            name_date_var = "ds",
+                            name_target_var = "y",
+                            grwth = 'logistic', 
+                            chg_point_prior = 0.5, 
+                            to_plot = F){
+  
+  tryCatch( {
+    df_forecasted_series_out <- data.frame()
+    df_cv_performance_out <- data.frame()
+    
+    df_in_prophet <- df_response
+    
+    country <- df_in_prophet$countrycode[1]
+    
+    colnames(df_in_prophet) <- str_replace(colnames(df_in_prophet), 
+                                           pattern = name_date_var, 
+                                           replacement = "ds")
+    colnames(df_in_prophet) <- str_replace(colnames(df_in_prophet), 
+                                           pattern = name_target_var, 
+                                           replacement = "y")
+    
+    if (grwth == 'logistic') {
+      # df_in_prophet$cap <- df_in_prophet$population / 2
+      df_in_prophet$cap <- max(df_in_prophet$deaths) * 3
+      df_in_prophet$floor <- 0
+    }
+    
+    # construct future (whole date period):
+    future <- df_in_prophet
+    # now past (all but the last 7 days):
+    df_past <- df_in_prophet %>% filter(ds < (max(ds)-7))
+    
+    
+    ## Forecasting response with no extra regressors ----
+    
+    m <- prophet(changepoint.range = 0.98,
+                 changepoint.prior.scale = chg_point_prior,
+                 growth = grwth,
+                 yearly.seasonality = F,
+                 weekly.seasonality = F,
+                 daily.seasonality = F)
+    
+    m <- fit.prophet(m, df_past)
+    
+    forecast <- predict(m, future) %>% 
+      mutate(yhat_lower = pmax(yhat_lower, 0)) # to avoid negative values in CI
+    
+    
+    if (to_plot) {
+      # p_compo <- prophet_plot_components(m, forecast) 
+      # ggsave(plot = p_compo,
+      #        filename =  paste0(out_path_forecast_response, "Plots-Pred-vs-Actual-Components/", 
+      #                           country, "-prophet-forecast-components.png"),
+      #        width = 7, height = 5)
+      
+      p_forecast <- plot(m, forecast) + add_changepoints_to_plot(m) + theme_light() +
+        labs(title = paste0(country, "-series forecast"), x = "Date", y = "Number of deaths")
+      ggsave(plot = p_forecast,
+             filename =  paste0(out_path_forecast_response, "Plots-Pred-vs-Actual/", 
+                                country, "-prophet-forecast.png"),
+             width = 7, height = 5)
+    }
+    
+    ## save forecasting:
+    df_forecasted_series_out <- forecast %>% 
+      select(ds, yhat, yhat_lower, yhat_upper, trend, trend_lower, trend_upper) %>% 
+      left_join(df_in_prophet, by = "ds") %>% 
+      mutate(date = ds) %>% 
+      select(!ds)
+    
+    write.csv(df_forecasted_series_out, 
+              file = paste0(out_path_forecast_response, "PlotData/", 
+                            country, "-prophet-forecast.csv"))
+    
+    ## CV forecasting ----
+    df.cv <- cross_validation(m, initial = round(nrow(forecast)*0.7), period = 2, horizon = 7, units = 'days')
+    df.p <- performance_metrics(df.cv)
+    df_cv_performance_out <- df.p
+    
+    df.p <- pivot_longer(data = df.p, 
+                         cols = colnames(df.p)[-1],
+                         names_to = "performance", values_to = "value")
+    
+    
+    if (to_plot) {
+      p_performance <- ggplot(data = df.p, 
+                              aes(x = horizon, y = value, group = performance)) +
+        facet_wrap(~performance, scales = "free_y") + 
+        geom_line( size = 1, alpha = 0.4) +
+        geom_point( size = 2, alpha = 0.8) +
+        theme_light() +
+        labs(title = paste0(country, ": cv-performance")) +
+        ylab("Performance") + xlab("Horizon") +
+        theme(legend.position = "bottom") 
+      ggsave(plot = p_performance,
+             filename =  paste0(out_path_forecast_response, "Plots-CV-Performance/", 
+                                country, "-cv-performance.png"),
+             width = 7, height = 5)
+    }
+    
+    
+    return(list(df_cv_performance = df_cv_performance_out, 
+                df_forecasted_series = df_forecasted_series_out))
+  }, error=function(cond) {
+    
+    message("Error!")
+    return(NA)
+  }
+    
+  )
+  
+  
+  
+}
+
+## DO for different countries ----
+
+file_in_deaths <- "../data/estimates-confirmed/PlotData/"
+file_in_deaths_pattern <- ".*-estimate.csv"
+
+out_path_forecast_response <- "../data/estimates-prophet-deaths/"
+
+files <- dir(file_in_deaths, pattern = file_in_deaths_pattern)
+
+for (file in files) {
+  iso_code_country <- substr(file, 1, 2)
+  cat("doing ", iso_code_country, ": ")
+  
+  
+  ## Load number of deaths ----
+  
+  df_deaths <-
+    read.csv(paste0(file_in_deaths, file)) %>%
+    select(!X) %>% 
+    mutate(date = as.Date(date)) %>%
+    mutate(y = deaths) %>%
+    mutate(y = rollmean(y, 1, fill = NA)) %>%
+    mutate(deaths = round(y)) %>% # CBM change
+    filter(!is.na(y))
+  
+  df_out_prophet <- propheting_data(df_response = df_deaths, 
+                              name_date_var = "date",
+                              name_target_var = "y",
+                              grwth = 'logistic', 
+                              chg_point_prior = 0.5, 
+                              to_plot = T)
+  
+  # cat("----> ", iso_code_country, ": ")
+  # message("success ")
+}
