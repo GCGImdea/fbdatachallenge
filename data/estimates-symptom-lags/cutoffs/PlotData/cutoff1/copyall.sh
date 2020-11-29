b7e55124164666a1392fdfb5c5a9a796aebf2b39
Davide Frey <davide.frey@gmail.com>
2020-11-29 10:40:24 +0000

    first massive forecast results in cutoffs/PlotData/cutoffs*/*

---
+++ data/estimates-symptom-lags/cutoffs/PlotData/cutoff1/copyall.sh
@@ -0,0 +1,3 @@
+docker cp -a coronasurveys:/home/fbdatachallenge-c1/data/estimates-symptom-lags/cutoffs/PlotData . 
+docker cp -a coronasurveys:/home/fbdatachallenge-c1/data/estimates-symptom-lags/cutoffs/Plots .
+
---
+++ data/estimates-symptom-lags/cutoffs/PlotData/cutoff15/copyall.sh
@@ -0,0 +1,3 @@
+docker cp -a coronasurveys:/home/fbdatachallenge/data/estimates-symptom-lags/cutoffs/PlotData . 
+docker cp -a coronasurveys:/home/fbdatachallenge/data/estimates-symptom-lags/cutoffs/Plots .
+
