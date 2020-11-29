108675ba1588e2978bbccf6bed2f3b87a09a25aa
Davide Frey <frey@quark-3.local>
2020-11-29 17:36:32 +0100

    fixed bug in lauch script

--- scripts/runManyLags.sh
+++ scripts/runManyLags.sh
@@ -7,14 +7,14 @@ for PEN in TRUE FALSE; do
 		for MILAG in 7 ; do
 		    for MXLAG in 60 ; do
 			for SIGTOMATCH in cases ; do
-			    #			    for SIGTOTRY in signals_umd "c(signals_umd,signals_ccfr)" "c(signals_umd,signals_ccfr,signals_nsum)"; do
-			    for SIGTOTRY in signals_umd;  do 
+			    for SIGTOTRY in signals_umd "c(signals_umd,signals_ccfr)" "c(signals_umd,signals_ccfr,signals_nsum)"; do
+			    #for SIGTOTRY in signals_umd;  do 
 				for FIRSTCUTOFF in 2020-9-10; do
 				    for LASTCUTOFF in 2020-11-10; do
 					for CUTOFFINTEVAL in 1 ; do
 
-					    sed 's,#PEN#,$PEN' runLag.sh | sed -i bak 's,#ALPHA#,$ALPHA' | sed -i bak 's,#RMCL#,$RMCL' | sed -i bak 's,#RMTH#,$RMTH' | sed -i bak 's,#MILAG#,$MILAG'| sed -i bak 's,#MXLAG#,$MXLAG'| sed -i bak 's,#SIGTOMATCH#,$SIGTOMATCH'    | sed -i bak 's,#SIGTOTRY#,$SIGTOTRY' | sed -i bak 's,#FIRSTCUTOFF#,$FIRSTCUTOFF'| sed -i bak 's,#LASTCUTOFF#,$LASTCUTOFF' | sed -i bak 's,#CUTOFFINTERVAL#,$CUTOFFINTERVAL' > runlag-pen$PEN$ALPHA-rmcl$RMCL$RMTH-lag$MILAG-$MXLAG-$SIGTOMATCH-$SIGTOTRY-$FIRSTCUTOFF-$LASTCUTOFF-$CUTOFFINTERVAL.R
-					    R CMD BATCH runlag-pen$PEN$ALPHA-rmcl$RMCL$RMTH-lag$MILAG-$MXLAG-$SIGTOMATCH-$SIGTOTRY-$FIRSTCUTOFF-$LASTCUTOFF-$CUTOFFINTERVAL.R &
+					    sed 's,#PEN#,$PEN,g' runlag.R | sed  's,#ALPHA#,$ALPHA,g' | sed  's,#RMCL#,$RMCL,g' | sed  's,#RMTH#,$RMTH,g' | sed  's,#MILAG#,$MILAG,g'| sed  's,#MXLAG#,$MXLAG,g'| sed  's,#SIGTOMATCH#,$SIGTOMATCH,g'    | sed  's,#SIGTOTRY#,$SIGTOTRY,g' | sed  's,#FIRSTCUTOFF#,$FIRSTCUTOFF,g'| sed  's,#LASTCUTOFF#,$LASTCUTOFF,g' | sed  's,#CUTOFFINTERVAL#,$CUTOFFINTERVAL,g' > runlag-pen$PEN$ALPHA-rmcl$RMCL$RMTH-lag$MILAG-$MXLAG-$SIGTOMATCH-$SIGTOTRY-$FIRSTCUTOFF-$LASTCUTOFF-$CUTOFFINTERVAL.R
+	#				    R CMD BATCH runlag-pen$PEN$ALPHA-rmcl$RMCL$RMTH-lag$MILAG-$MXLAG-$SIGTOMATCH-$SIGTOTRY-$FIRSTCUTOFF-$LASTCUTOFF-$CUTOFFINTERVAL.R &
 					    
 					done
 				    done
