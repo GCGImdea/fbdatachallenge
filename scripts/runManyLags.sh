0ab430a4eb4b4a4b8c18af78703a096e00b6d499
Davide Frey <frey@quark-3.local>
2020-11-29 17:57:19 +0100

    fixed other bug

--- scripts/runManyLags.sh
+++ scripts/runManyLags.sh
@@ -7,14 +7,14 @@ for PEN in TRUE FALSE; do
 		for MILAG in 7 ; do
 		    for MXLAG in 60 ; do
 			for SIGTOMATCH in cases ; do
-			    for SIGTOTRY in signals_umd 'c(signals_umd,signals_ccfr)' 'c(signals_umd,signals_ccfr,signals_nsum)'; do
+			    for SIGTOTRY in signals_umd; do
 			    #for SIGTOTRY in signals_umd;  do 
 				for FIRSTCUTOFF in 2020-9-10; do
 				    for LASTCUTOFF in 2020-11-10; do
 					for CUTOFFINTEVAL in 1 ; do
 
 					    sed "s,#PEN#,$PEN,g" runlag.R | sed  "s,#ALPHA#,$ALPHA,g" | sed  "s,#RMCL#,$RMCL,g" | sed  "s,#RMTH#,$RMTH,g" | sed  "s,#MILAG#,$MILAG,g"| sed  "s,#MXLAG#,$MXLAG,g"| sed  "s,#SIGTOMATCH#,$SIGTOMATCH,g"    | sed  "s,#SIGTOTRY#,$SIGTOTRY,g" | sed  "s,#FIRSTCUTOFF#,$FIRSTCUTOFF,g"| sed  "s,#LASTCUTOFF#,$LASTCUTOFF,g" | sed  "s,#CUTOFFINTERVAL#,$CUTOFFINTERVAL,g" > runlag-pen$PEN$ALPHA-rmcl$RMCL$RMTH-lag$MILAG-$MXLAG-$SIGTOMATCH-$SIGTOTRY-$FIRSTCUTOFF-$LASTCUTOFF-$CUTOFFINTERVAL.R
-	#				    R CMD BATCH runlag-pen$PEN$ALPHA-rmcl$RMCL$RMTH-lag$MILAG-$MXLAG-$SIGTOMATCH-$SIGTOTRY-$FIRSTCUTOFF-$LASTCUTOFF-$CUTOFFINTERVAL.R &
+					    R CMD BATCH runlag-pen$PEN$ALPHA-rmcl$RMCL$RMTH-lag$MILAG-$MXLAG-$SIGTOMATCH-$SIGTOTRY-$FIRSTCUTOFF-$LASTCUTOFF-$CUTOFFINTERVAL.R &
 					    
 					done
 				    done
